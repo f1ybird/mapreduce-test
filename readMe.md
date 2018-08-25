4af2683de92ecef2d5ce12520dc11bff2927ed15
kevin_love_it@163.com <kevin_love_it@163.com>
2018-08-26 00:29:42 +0800

    mapreduce demo

---
+++ readMe.md
@@ -0,0 +1,4 @@
+### hadoop2.6.5分布式环境
+
+#### mapreduce demo
+##### 1.wordCount
---
+++ src/main/java/com/kevin/mapreduce/demo/Excise.java
@@ -0,0 +1,28 @@
+package com.kevin.mapreduce.demo;
+ 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+ 
+import java.net.URI;
+ 
+/**
+ * describe  : 列出/input下的所有文件
+ * creat_user: zhangkai
+ * creat_time: 2018/8/25 20:57
+ * email     : kevin_love_it@163.com
+ **/
+public class Excise {
+ 
+    public static void main(String[] args)throws Exception{
+        String uri = "hdfs://192.168.72.39:9000";
+        Configuration conf = new Configuration();
+        FileSystem fs = FileSystem.get(URI.create(uri),conf);
+        //列出hdfs上 /input 目录下的所有文件
+        FileStatus[] statuses = fs.listStatus(new Path("/input"));
+        for (FileStatus status:statuses){
+            System.out.println(status);
+        }
+    }
+}
