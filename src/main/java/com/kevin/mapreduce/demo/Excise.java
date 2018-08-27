de84efefe9e464401b85064b484e9be779d5313d
kevin_love_it@163.com <kevin_love_it@163.com>
2018-08-27 22:51:31 +0800

    add logback.xml

--- src/main/java/com/kevin/mapreduce/demo/Excise.java
+++ src/main/java/com/kevin/mapreduce/demo/Excise.java
@@ -1,10 +1,11 @@
 package com.kevin.mapreduce.demo;
- 
+
+import com.kevin.mapreduce.utils.LoggerUtil;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
- 
+
 import java.net.URI;
  
 /**
@@ -14,12 +15,12 @@ import java.net.URI;
  * email     : kevin_love_it@163.com
  **/
 public class Excise {
- 
+
     public static void main(String[] args)throws Exception{
+        LoggerUtil.info("列出hdfs上 /input 目录下的所有文件");
         String uri = "hdfs://192.168.72.39:9000";
         Configuration conf = new Configuration();
         FileSystem fs = FileSystem.get(URI.create(uri),conf);
-        //列出hdfs上 /input 目录下的所有文件
         FileStatus[] statuses = fs.listStatus(new Path("/input"));
         for (FileStatus status:statuses){
             System.out.println(status);
